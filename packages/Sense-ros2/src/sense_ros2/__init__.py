78cd27404a2898ae0bc44e41ce014d0d2640cf45
Promise Emmanuel Oluwadare <95595405+promiseeuler@users.noreply.github.com>
2026-09-18 19:04:12 +0100

    chore(ros2): align adapter version

--- packages/Sense-ros2/src/sense_ros2/__init__.py
+++ packages/Sense-ros2/src/sense_ros2/__init__.py
@@ -6,4 +6,4 @@ from .client import Ros2Client
 from .mapping import Ros2SenseBridge, RosTopicMapping
 
 __all__ = ["Ros2Client", "Ros2SenseBridge", "RosTopicMapping"]
-__version__ = "0.2.0"
+__version__ = "0.3.0"
